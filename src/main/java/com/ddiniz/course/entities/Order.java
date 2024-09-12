44eaa394febf740bc35de24a11aa1a7b95ff85e3
Derek Diniz <drk.diniz@gmail.com>
2024-09-12 17:56:33 -0300

    OrderStatus enum

--- src/main/java/com/ddiniz/course/entities/Order.java
+++ src/main/java/com/ddiniz/course/entities/Order.java
@@ -1,8 +1,12 @@
 package com.ddiniz.course.entities;
 
 import java.io.Serializable;
+
 import java.time.Instant;
 import java.util.Objects;
+
+import com.ddiniz.course.entities.enums.OrderStatus;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -20,7 +24,13 @@ public class Order implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",
+			timezone = "GMT")
 	private Instant moment;
+	
+	private Integer orderStatus;
+	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
@@ -28,11 +38,22 @@ public class Order implements Serializable{
 	public Order() {	
 	}
 	
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+		this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	public Long getId() {
